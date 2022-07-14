9af17bb6b6acedb030710677c98d5cd10f84ef8a
Yousef <mr.ysaj@gmail.com>
2022-07-14 10:40:09 +0430

    [UPDATE] add usa_table to table.py

--- table.py
+++ table.py
@@ -22,3 +22,12 @@ def find_flag(element):
     """ find the flag of each id """
     flag_name = element.find("td", class_="left flagCur noWrap").text.strip()
     return flag_name
+
+
+def usa_table(table):
+    """only show USA event"""
+    for index, flag in enumerate(table.values):
+        if not flag[1] == "USD":
+            table = table.drop(index)
+    table = table.reset_index(drop=True)
+    return table
