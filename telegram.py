711aec2dbe3e77d8fba4f6f12d44ca8e6850d36b
Yousef <mr.ysaj@gmail.com>
2022-07-14 10:49:57 +0430

    [UPDATE] the telegram.py was updated

--- telegram.py
+++ telegram.py
@@ -1,15 +1,44 @@
 import requests
+import hashlib
 
 
-def send_massage():
+with open("event.json", 'r', encoding='utf-8') as e:
+    persian = json.load(e)
+
+
+def send_massage(loc, checklist):
     """send a massage to channel by telegram bot"""
-
+    for pr in persian:
+        if pr in loc[3]:
+            persian_name = persian[pr]
+            break
+    event = loc[3]
+    actual = loc[4]
+    forecast = loc[5]
+    previous = loc[6]
     base_url = "https://api.telegram.org/bot5334404508:AAFD-Vkaghr_BLOkC5n1Sy_XwFzKl3_4DSo/sendMessage"
     parameters = {
-        "chat_id": "@ZeroHedge2",
-        "text": "This is test API"
+        "chat_id": "@Amiramidiyan",
+        "text": f"""
+✅{persian_name} 
+
+{event}
+
+✔️ واقعی---->{actual} 
+✔️ پیش بینی---->{forecast} 
+✔️ قبلی---->{previous} 
+
+
+
+📍@Amiramidiyan📍
+"""
     }
-    resp = requests.get(url=base_url, data=parameters)
-    return resp
-
-
+    hash_massage = hashlib.sha256(parameters["text"].encode("utf-8")).hexdigest()
+    if not hash_massage in checklist:
+        try:
+            resp = requests.get(url=base_url, data=parameters)
+            print(parameters["text"])
+            print(colored("the massage was sent to bot", "green"))
+        except Exception as e:
+            print(e)
+    return hash_massage
